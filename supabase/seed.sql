0d62c7d4ed033f21555add2fae5ac1eb9aa5d02e
jason-devs <jasondevs.mail@gmail.com>
2026-06-09 17:37:57 +0100

    feature: improved schema for endings

---
+++ supabase/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO endings ("ending_number", "nickname", "audio_path", "info")
+VALUES
+    (1, 'the first one', '1.mp3', 'you need to think of how the first one should go. then think about how the first one should feel. then how the first one should sound.'),
+    (2, 'the second one', '2.mp3', 'you need to think of how the second one should go. then think about how the second one should feel. then how the second one should sound.'),
+    (3, 'the third one', '3.mp3', 'you need to think of how the third one should go. then think about how the third one should feel. then how the third one should sound.');
+
