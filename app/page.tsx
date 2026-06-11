528dbcac4e6b1e22f3f81a2f91b2664161737769
jason-devs <jasondevs.mail@gmail.com>
2026-06-11 16:03:59 +0100

    feature: bad ending card added and ui colors removed

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,9 @@ import { Ending } from "@/lib/types/Ending";
 export default async function Page() {
   const getAudioUrl = (path: string) =>
     supabase.storage.from("audio").getPublicUrl(path).data.publicUrl;
+  const badAudioUrl = supabase.storage.from("audio").getPublicUrl("bad.mp3")
+    .data.publicUrl;
+
   const { data: rawEndings, error } = await supabase.from("endings").select();
 
   if (error) {
@@ -21,21 +24,13 @@ export default async function Page() {
     },
   );
 
+  // HACK: i'm prop drilling the badAudioUrl -->
+
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-8">
         <TypographyH1>Outropedia</TypographyH1>
-        <InfoBox />
-        {/* <TypographyP> */}
-        {/*   For all those rock and roll bands with no idea how to finish what they */}
-        {/*   start. Here's some inspiration, plus a way to put a name to a ending */}
-        {/*   so you can stop saying "nah bro! it went duh-duh du-dum!", and start */}
-        {/*   saying, it's rock and roll ending #5. */}
-        {/* </TypographyP> */}
-        {/* <TypographyP> */}
-        {/*   Also, so you can avoid ending #101 "the f*** up": */}
-        {/* </TypographyP> */}
-        {/* <audio controls src={getAudioUrl("bad.mp3")} className="mt-8" /> */}
+        <InfoBox badAudioUrl={badAudioUrl} />
       </div>
       <div className="flex flex-row w-full md:justify-end">
         <SearchEndingsBar />
